7f307e03434e8eaae929a9370a79a69256dfcada
Mukhammadyusuf <muhammadusufhajdarov82@gmail.com>
2025-12-24 22:20:10 +0500

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
